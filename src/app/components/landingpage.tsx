440cb7c7b926cb2d001488ffd7403d62f243e870
Tithi Shah <shahtithi21@gmail.com>
2025-05-28 17:37:17 +0530

    update:- landing page

---
+++ src/app/components/landingpage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+    <section className="bg-[#090b23] h-[180vw] lg:h-[65vw] w-full px-10">
+        <div className="pt-9 text-[2rem] lg:text-[4rem] font-[1000]">Get Your Resume In Minutes</div>
+        <div className="pt-9 text-[2rem] lg:text-[1.5rem] font-[400]">Build, edit, and export your resume seamlessly with our professional templates.<br/>
+        Choose from modern, clean, and creative layouts tailored to your career path.<br/>
+        Customize every detail to stand out from the crowd.<br/>
+        Download your resume instantly — no watermarks, no hassle.<br/>
+        Make your first impression count with ResumeBuilder Pro.</div>
+    </section>
+    </>
+  )
+}
+
+export default LandingPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
+import LandingPage from "./components/landingpage";
 export default function Home() {
   return (
     <>
       {/* Navbar */}
-      <div className="bg-slate-800 py-12 border-t flex flex-col lg:flex-row justify-between items-center">
-            <div className="mb-4 lg:mb-0 pl-9">
+      <div className="bg-slate-900 py-12 border-b border-slate-800 flex flex-col lg:flex-row justify-between items-center">
+            <div className="mb-0 lg:pl-9">
               <h1 className="text-2xl lg:text-3xl font-semibold text-white">
                 ResumeBuilder Pro
               </h1>
             </div>
-            <div className="flex flex-row gap-5 pr-9">
+            <div className="hidden lg:flex flex-row gap-5 pr-9 justify-center items-center">
               <button className="border-gray-300 border-[0.1rem] rounded-lg px-10 py-1 font-bold hover:bg-slate-600">Sign In</button>
               <button className="border-gray-300 border-[0.1rem] rounded-lg px-10 py-1 font-bold hover:bg-slate-600">Sign Up</button>
             </div>
       </div>
-
+      <LandingPage />
       {/* Footer */}
       <footer className="py-12 bg-slate-900 border-t border-slate-800 flex flex-col lg:flex-row justify-between items-center lg:px-9">
             <div className="mb-4 lg:mb-0">
